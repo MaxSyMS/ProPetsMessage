2d4fcbc63757188476468028475e86e22de54a13
SyMS <SyMS@10.0.0.19>
2020-02-22 12:58:04 +0200

    commit 1

---
+++ ProPetsMessage/src/main/java/propets/ProPetsMessageApplication.java
@@ -0,0 +1,13 @@
+package propets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProPetsMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProPetsMessageApplication.class, args);
+	}
+
+}
